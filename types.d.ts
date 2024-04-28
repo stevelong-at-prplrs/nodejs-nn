47c7668ceee77dd65c0cce0acd7822a0a80050c8
stevelong-at-prplrs <76882223+stevelong-at-prplrs@users.noreply.github.com>
2024-04-28 18:25:22 -0500

    adding and using types

---
+++ types.d.ts
@@ -0,0 +1,39 @@
+type NodeId = number;
+type Weight = number;
+type Bias = number;
+
+export type NodeWeight = [NodeId, Weight, Bias?];
+
+export type NNNode = NodeWeight[];
+
+export type Layer = NNNode[];
+
+export type LayerInputArray = number[];
+
+export type NeuralNetwork = Layer[];
+
+export type InputArrayType = "Network" | "Inputs" | "Assertions";
+
+// type ActivationFunction = (x: number) => number;
+
+// type ActivationFunctions = {
+//   [key: string]: ActivationFunction;
+// };
+
+// type ActivationFunctionName = keyof ActivationFunctions;
+
+// type ActivationFunctionOptions = {
+//   [key: string]: number;
+// };
+
+// type ActivationFunctionMap = {
+//   [key: string]: ActivationFunctionOptions;
+// };
+
+// type ActivationFunctionMapName = keyof ActivationFunctionMap;
+
+// type ActivationFunctionMapOptions = {
+//   [key: string]: ActivationFunctionOptions;
+// };
+
+// type ActivationFunctionMapOptionsName = keyof ActivationFunctionMapOptions;
