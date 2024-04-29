35d7aa95b12d91da338e5a4ffaff08a4fe449887
stevelong-at-prplrs <76882223+stevelong-at-prplrs@users.noreply.github.com>
2024-04-28 19:55:42 -0500

    add typescript

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { fetchInputArrays, runCa, runTests, useCommandLineArgs } from './functions.js';
+import { InputArrayType } from './types.js';
+
+const [runType, networkFilename, inputsFilename, thirdCommandLineParam] = useCommandLineArgs();
+
+if (!runType || !networkFilename || !inputsFilename || !thirdCommandLineParam) {
+    console.error("Usage: node main.js <test|ca> <networkFilename> <inputsFilename> <assertionsFilename|numberOfRepetitions>");
+    process.exit(1);
+}
+
+const generations = parseInt(thirdCommandLineParam);
+const fileTuples = [[networkFilename, "Network"], [inputsFilename, "Inputs"], isNaN(generations) ? [thirdCommandLineParam, "Assertions"] : undefined].filter((tuple) => tuple !== undefined) as [string, InputArrayType][];
+// if (isNaN(generations)) fileTuples.push([thirdCommandLineParam, "Assertions"]);
+const [network, inputs, assertions] = fetchInputArrays(fileTuples);
+
+if (runType === "test") {
+    runTests(network, inputs, assertions);
+} else if (runType === "ca") {
+    runCa(network, inputs, generations);
+} else process.exit(1);
